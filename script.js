2d734ed136b200f253d9b798a00cce27d59c7c07
Abhishek Hembrom <abhishekhembrom200@gmail.com>
2026-09-06 21:13:53 +0530

    test5

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ const TYPER_SPEED = 80; // ms per character — lower = faster
 (function setupLock() {
   const now    = new Date();
   const unlock = new Date(now);
-  unlock.setHours(23, 50, 0, 0);
+  unlock.setHours(21, 14, 0, 0);
 
   const hrsEl      = document.getElementById("lock-hrs");
   const minsEl     = document.getElementById("lock-mins");
